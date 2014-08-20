8fa95b22cfe422c5740d681fa1744df12a7f3d23
Yaroslav <wenqeer@gmail.com>
2014-08-21 00:36:40 +0300

    first tests

--- test/kue-mq_test.js
+++ test/kue-mq_test.js
@@ -1,10 +1,42 @@
-/*global describe,it*/
+/*global describe,it,beforeEach,afterEach*/
 'use strict';
 var assert = require('assert'),
-  kueMq = require('../lib/kue-mq.js');
+  kueMq = require('../lib/kue-mq.js'),
+  redisConf = {
+    prefix: 'q',
+    redis: {
+        port: 6379,
+        host: '146.185.188.32'
+    }
+  },
+  serverMethods = {
+    run: function(data, cb) {
+      cb(null, data);
+    }
+  };
 
 describe('kue-mq node module.', function() {
-  it('must be awesome', function() {
-    assert( kueMq .awesome(), 'awesome');
+  var s1, s2;
+
+  beforeEach(function() {
+    s1 = kueMq(redisConf, 'test1', serverMethods);
+    s2 = kueMq(redisConf, 'test2', serverMethods);
+  });
+
+  afterEach(function() {
+    s1 = s2 = null;
+  });
+
+  it('first server must take answer from second', function(done) {
+    s1
+      .send('test2', 'run', {'foo': 'bar'})
+      .then(function(res) {
+        assert(res.foo, 'bar');
+        done();
+      },
+      function(err) {
+        console.error(err);
+        done(err);
+      });
   });
 });
